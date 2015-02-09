2e7e42810a1bd3cdbec1c3a38a4a52f41d4e47a3
Aaron Jacobs <jacobsa@google.com>
2015-02-09 14:14:32 +1100

    Added auth wiring code.

--- gcs/integration_test.go
+++ gcs/integration_test.go
@@ -12,24 +12,93 @@
 package gcs_test
 
 import (
+	"flag"
+	"log"
+	"net/http"
 	"testing"
 
 	"github.com/jacobsa/gcloud/gcs"
+	"github.com/jacobsa/gcloud/oauthutil"
 	. "github.com/jacobsa/ogletest"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/storage/v1"
 )
 
 func TestOgletest(t *testing.T) { RunTests(t) }
 
 ////////////////////////////////////////////////////////////////////////
-// Helpers
+// Wiring code
 ////////////////////////////////////////////////////////////////////////
+
+var fBucketName = flag.String("bucket", "", "Empty bucket to use for storage.")
+
+func getHttpClientOrDie() *http.Client {
+	// Set up a token source.
+	config := &oauth2.Config{
+		ClientID:     "517659276674-6am1g9cau0bc0pbre6d59d9b5168b1tr.apps.googleusercontent.com",
+		ClientSecret: "YXtgLuEVJoTlG-3D-4UoGEGq",
+		RedirectURL:  "urn:ietf:wg:oauth:2.0:oob",
+		Scopes:       []string{storage.DevstorageFull_controlScope},
+		Endpoint:     google.Endpoint,
+	}
+
+	tokenSource, err := oauthutil.NewTerribleTokenSource(
+		config,
+		flag.Lookup("auth_code"),
+		".gcs_integration_test.token_cache.json")
+
+	if err != nil {
+		log.Fatalln("oauthutil.NewTerribleTokenSource:", err)
+	}
+
+	// Ensure that we fail early if misconfigured, by requesting an initial
+	// token.
+	log.Println("Requesting initial OAuth token.")
+	if _, err := tokenSource.Token(); err != nil {
+		log.Fatalln("Getting initial OAuth token:", err)
+	}
+
+	// Create the HTTP transport.
+	transport := &oauth2.Transport{
+		Source: tokenSource,
+	}
+
+	return &http.Client{Transport: transport}
+}
+
+func getBucketNameOrDie() string {
+	s := *fBucketName
+	if s == "" {
+		log.Fatalln("You must set --bucket.")
+	}
+
+	return s
+}
 
 // Return a bucket based on the contents of command-line flags, exiting the
 // process if misconfigured.
-func getBucketOrDie() gcs.Bucket
+func getBucketOrDie() gcs.Bucket {
+	// A project ID is apparently only needed for creating and listing buckets,
+	// presumably since a bucket ID already maps to a unique project ID (cf.
+	// http://goo.gl/Plh3rb). This doesn't currently matter to us.
+	const projectId = "some_project_id"
+
+	// Set up a GCS connection.
+	conn, err := gcs.NewConn(projectId, getHttpClientOrDie())
+	if err != nil {
+		log.Fatalf("gcs.NewConn: %v", err)
+	}
+
+	// Open the bucket.
+	return conn.GetBucket(getBucketNameOrDie())
+}
 
 // Delete everything in the bucket, exiting the process on failure.
-func deleteAllObjectsOrDie(b gcs.Bucket)
+func deleteAllObjectsOrDie(b gcs.Bucket) {
+	log.Println("ASDASDASD")
+	log.Fatalln("TODO(jacobsa): Implement deleteAllObjectsOrDie.")
+}
 
 ////////////////////////////////////////////////////////////////////////
 // Listing
