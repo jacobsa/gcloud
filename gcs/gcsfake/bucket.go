1774918b390dabf0ca5a8fc1e2205be19f78082e
Aaron Jacobs <jacobsa@google.com>
2015-02-12 16:15:52 +1100

    Added a stub implementation.

--- gcs/gcsfake/bucket.go
+++ gcs/gcsfake/bucket.go
@@ -4,6 +4,7 @@
 package gcsfake
 
 import (
+	"errors"
 	"io"
 
 	"github.com/jacobsa/gcloud/gcs"
@@ -20,20 +21,30 @@ type bucket struct {
 	name string
 }
 
-func (b *bucket) Name() string
+func (b *bucket) Name() string {
+	return b.name
+}
 
 func (b *bucket) ListObjects(
 	ctx context.Context,
-	query *storage.Query) (*storage.Objects, error)
+	query *storage.Query) (*storage.Objects, error) {
+	return nil, errors.New("TODO: Implement ListObjects.")
+}
 
 func (b *bucket) NewReader(
 	ctx context.Context,
-	objectName string) (io.ReadCloser, error)
+	objectName string) (io.ReadCloser, error) {
+	return nil, errors.New("TODO: Implement NewReader.")
+}
 
 func (b *bucket) NewWriter(
 	ctx context.Context,
-	attrs *storage.ObjectAttrs) (gcs.ObjectWriter, error)
+	attrs *storage.ObjectAttrs) (gcs.ObjectWriter, error) {
+	return nil, errors.New("TODO: Implement NewWriter.")
+}
 
 func (b *bucket) DeleteObject(
 	ctx context.Context,
-	name string) error
+	name string) error {
+	return errors.New("TODO: Implement DeleteObject.")
+}
