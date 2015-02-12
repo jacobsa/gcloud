ff05deb819838c200d91e8ee99c0b0a3513b6f59
Aaron Jacobs <jacobsa@google.com>
2015-02-12 16:13:16 +1100

    Added a test for the fake bucket.

---
+++ gcs/gcsfake/bucket_test.go
@@ -0,0 +1,20 @@
+// Copyright 2015 Google Inc. All Rights Reserved.
+// Author: jacobsa@google.com (Aaron Jacobs)
+
+package gcsfake_test
+
+import (
+	"testing"
+
+	"github.com/jacobsa/gcloud/gcs"
+	"github.com/jacobsa/gcloud/gcs/gcstesting"
+	"github.com/jacobsa/ogletest"
+)
+
+func TestOgletest(t *testing.T) { ogletest.RunTests(t) }
+
+func init() {
+	gcstesting.RegisterBucketTests(func() gcs.Bucket {
+		return gcstest.NewFakeBucket("some_bucket")
+	})
+}
