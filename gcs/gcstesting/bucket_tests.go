44275b013870ab38ba83405cb031e7cdeb955fb2
Aaron Jacobs <jacobsa@google.com>
2015-02-12 15:26:31 +1100

    Trimmed stuff that isn't needed in bucket_tests.go.

--- gcs/gcstesting/bucket_tests.go
+++ gcs/gcstesting/bucket_tests.go
@@ -1,181 +1,32 @@
 // Copyright 2015 Google Inc. All Rights Reserved.
 // Author: jacobsa@google.com (Aaron Jacobs)
 //
-// An integration test that uses the real GCS. Run it with appropriate flags as
-// follows:
-//
-//     go test -bucket <bucket name>
-//
-// The bucket's contents are not preserved.
-//
-// The first time you run the test, it may die with a URL to visit to obtain an
-// authorization code after authorizing the test to access your bucket. Run it
-// again with the "-auth_code" flag afterward.
+// Tests registered by RegisterBucketTests.
 
-package gcs_test
+package gcstesting
 
 import (
 	"bytes"
 	"crypto/md5"
 	"encoding/hex"
-	"flag"
-	"fmt"
 	"hash/crc32"
 	"io"
 	"io/ioutil"
-	"log"
 	"math"
-	"net/http"
 	"strings"
-	"testing"
 	"time"
 
 	"github.com/jacobsa/gcloud/gcs"
-	"github.com/jacobsa/gcloud/oauthutil"
 	"github.com/jacobsa/gcloud/syncutil"
 	. "github.com/jacobsa/oglematchers"
 	. "github.com/jacobsa/ogletest"
 	"golang.org/x/net/context"
-	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/google"
-	storagev1 "google.golang.org/api/storage/v1"
 	"google.golang.org/cloud/storage"
 )
-
-func TestOgletest(t *testing.T) { RunTests(t) }
-
-////////////////////////////////////////////////////////////////////////
-// Wiring code
-////////////////////////////////////////////////////////////////////////
-
-var fBucket = flag.String("bucket", "", "Empty bucket to use for storage.")
-var fAuthCode = flag.String("auth_code", "", "Auth code from GCS console.")
-var fDebugHttp = flag.Bool("debug_http", false, "Dump information about HTTP requests.")
-
-func getHttpClientOrDie() *http.Client {
-	// Set up a token source.
-	config := &oauth2.Config{
-		ClientID:     "517659276674-k9tr62f5rpd1k6ivvhadq0etbu4gu3t5.apps.googleusercontent.com",
-		ClientSecret: "A6Xo63GDMRHmZ2TB7CO99lLN",
-		RedirectURL:  "urn:ietf:wg:oauth:2.0:oob",
-		Scopes:       []string{storagev1.DevstorageFull_controlScope},
-		Endpoint:     google.Endpoint,
-	}
-
-	tokenSource, err := oauthutil.NewTerribleTokenSource(
-		config,
-		flag.Lookup("auth_code"),
-		".gcs_integration_test.token_cache.json")
-
-	if err != nil {
-		log.Fatalln("oauthutil.NewTerribleTokenSource:", err)
-	}
-
-	// Ensure that we fail early if misconfigured, by requesting an initial
-	// token.
-	if _, err := tokenSource.Token(); err != nil {
-		log.Fatalln("Getting initial OAuth token:", err)
-	}
-
-	// Create the HTTP transport.
-	transport := &oauth2.Transport{
-		Source: tokenSource,
-		Base:   http.DefaultTransport,
-	}
-
-	if *fDebugHttp {
-		transport.Base = &debuggingTransport{wrapped: transport.Base}
-	}
-
-	return &http.Client{Transport: transport}
-}
-
-func getBucketNameOrDie() string {
-	s := *fBucket
-	if s == "" {
-		log.Fatalln("You must set --bucket.")
-	}
-
-	return s
-}
-
-// Return a bucket based on the contents of command-line flags, exiting the
-// process if misconfigured.
-func getBucketOrDie() gcs.Bucket {
-	// A project ID is apparently only needed for creating and listing buckets,
-	// presumably since a bucket ID already maps to a unique project ID (cf.
-	// http://goo.gl/Plh3rb). This doesn't currently matter to us.
-	const projectId = "some_project_id"
-
-	// Set up a GCS connection.
-	conn, err := gcs.NewConn(projectId, getHttpClientOrDie())
-	if err != nil {
-		log.Fatalf("gcs.NewConn: %v", err)
-	}
-
-	// Open the bucket.
-	return conn.GetBucket(getBucketNameOrDie())
-}
 
 ////////////////////////////////////////////////////////////////////////
 // Helpers
 ////////////////////////////////////////////////////////////////////////
-
-// List all object names in the bucket into the supplied channel.
-// Responsibility for closing the channel is not accepted.
-func listIntoChannel(ctx context.Context, b gcs.Bucket, objectNames chan<- string) error {
-	query := &storage.Query{}
-	for query != nil {
-		objects, err := b.ListObjects(ctx, query)
-		if err != nil {
-			return err
-		}
-
-		for _, obj := range objects.Results {
-			select {
-			case <-ctx.Done():
-				return ctx.Err()
-			case objectNames <- obj.Name:
-			}
-		}
-
-		query = objects.Next
-	}
-
-	return nil
-}
-
-// Delete everything in the bucket, exiting the process on failure.
-func deleteAllObjectsOrDie(ctx context.Context, b gcs.Bucket) {
-	bundle := syncutil.NewBundle(ctx)
-
-	// List all of the objects in the bucket.
-	objectNames := make(chan string, 10)
-	bundle.Add(func(ctx context.Context) error {
-		defer close(objectNames)
-		return listIntoChannel(ctx, b, objectNames)
-	})
-
-	// Delete the objects in parallel.
-	const parallelism = 10
-	for i := 0; i < parallelism; i++ {
-		bundle.Add(func(ctx context.Context) error {
-			for objectName := range objectNames {
-				if err := b.DeleteObject(ctx, objectName); err != nil {
-					return err
-				}
-			}
-
-			return nil
-		})
-	}
-
-	// Wait.
-	err := bundle.Join()
-	if err != nil {
-		panic("deleteAllObjectsOrDie: " + err.Error())
-	}
-}
 
 func createObject(
 	ctx context.Context,
@@ -235,79 +86,13 @@ func computeCrc32C(s string) uint32 {
 }
 
 ////////////////////////////////////////////////////////////////////////
-// HTTP debugging
-////////////////////////////////////////////////////////////////////////
-
-func readAllAndClose(rc io.ReadCloser) string {
-	// Read.
-	contents, err := ioutil.ReadAll(rc)
-	if err != nil {
-		panic(err)
-	}
-
-	// Close.
-	if err := rc.Close(); err != nil {
-		panic(err)
-	}
-
-	return string(contents)
-}
-
-// Read everything from *rc, then replace it with a copy.
-func snarfBody(rc *io.ReadCloser) string {
-	contents := readAllAndClose(*rc)
-	*rc = ioutil.NopCloser(bytes.NewBufferString(contents))
-	return contents
-}
-
-type debuggingTransport struct {
-	wrapped http.RoundTripper
-}
-
-func (t *debuggingTransport) RoundTrip(req *http.Request) (*http.Response, error) {
-	// Print information about the request.
-	fmt.Println("========== REQUEST ===========")
-	fmt.Println(req.Method, req.URL, req.Proto)
-	for k, vs := range req.Header {
-		for _, v := range vs {
-			fmt.Printf("%s: %s\n", k, v)
-		}
-	}
-
-	if req.Body != nil {
-		fmt.Printf("\n%s\n", snarfBody(&req.Body))
-	}
-
-	// Execute the request.
-	res, err := t.wrapped.RoundTrip(req)
-	if err != nil {
-		return res, err
-	}
-
-	// Print the response.
-	fmt.Println("========== RESPONSE ==========")
-	fmt.Println(res.Proto, res.Status)
-	for k, vs := range res.Header {
-		for _, v := range vs {
-			fmt.Printf("%s: %s\n", k, v)
-		}
-	}
-
-	if res.Body != nil {
-		fmt.Printf("\n%s\n", snarfBody(&res.Body))
-	}
-	fmt.Println("==============================")
-
-	return res, err
-}
-
-////////////////////////////////////////////////////////////////////////
 // Common
 ////////////////////////////////////////////////////////////////////////
 
 type BucketTest struct {
-	ctx    context.Context
-	bucket gcs.Bucket
+	getBucket func() gcs.Bucket
+	ctx       context.Context
+	bucket    gcs.Bucket
 }
 
 var _ SetUpInterface = &BucketTest{}
@@ -315,10 +100,7 @@ var _ SetUpInterface = &BucketTest{}
 func (t *BucketTest) SetUp(ti *TestInfo) {
 	// Create a context and bucket.
 	t.ctx = context.Background()
-	t.bucket = getBucketOrDie()
-
-	// Ensure that the bucket is clean.
-	deleteAllObjectsOrDie(t.ctx, t.bucket)
+	t.bucket = t.getBucket()
 }
 
 func (t *BucketTest) createObject(name string, contents string) error {
