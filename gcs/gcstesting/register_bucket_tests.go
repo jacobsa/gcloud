7596095e1076cf8d2c30f4ab2c73910554e9bff7
Aaron Jacobs <jacobsa@google.com>
2015-02-12 15:44:31 +1100

    Implemented registerTestSuite.

--- gcs/gcstesting/register_bucket_tests.go
+++ gcs/gcstesting/register_bucket_tests.go
@@ -3,16 +3,58 @@
 
 package gcstesting
 
-import "github.com/jacobsa/gcloud/gcs"
+import (
+	"reflect"
+
+	"github.com/jacobsa/gcloud/gcs"
+	"github.com/jacobsa/ogletest"
+)
 
 // An interface that all bucket tests must implement.
 type bucketTestSetUpInterface interface {
 	SetUpBucketTest(b gcs.Bucket)
 }
 
+func getSuiteName(prototype interface{}) string
+
+func getTestMethods(suitePrototype interface{}) []reflect.Method
+
 func registerTestSuite(
 	makeBucket func() gcs.Bucket,
-	prototype bucketTestSetUpInterface)
+	prototype bucketTestSetUpInterface) {
+	suiteType := reflect.TypeOf(prototype)
+
+	// We don't need anything fancy at the suite level.
+	var ts ogletest.TestSuite
+	ts.Name = getSuiteName(prototype)
+
+	// For each method, we create a test function.
+	for _, method := range getTestMethods(prototype) {
+		var tf ogletest.TestFunction
+
+		// Create an instance to be shared among SetUp and the test function itself.
+		var instance reflect.Value = reflect.New(suiteType)
+
+		// SetUp should create a bucket and then initialize the suite object,
+		// remembering that the suite implements bucketTestSetUpInterface.
+		tf.SetUp = func(*ogletest.TestInfo) {
+			bucket := makeBucket()
+			instance.Interface().(bucketTestSetUpInterface).SetUpBucketTest(bucket)
+		}
+
+		// The test function itself should simply invoke the method.
+		methodCopy := method
+		tf.Run = func() {
+			methodCopy.Func.Call([]reflect.Value{instance})
+		}
+
+		// Save the test function.
+		ts.TestFunctions = append(ts.TestFunctions, tf)
+	}
+
+	// Register the suite.
+	ogletest.Register(ts)
+}
 
 // Given a function that returns an initialized, empty bucket, register test
 // suites that exercise the buckets returned by the function with ogletest.
