24b24dffca00d9f8527754b602358237831a0022
Aaron Jacobs <jacobsa@google.com>
2015-02-12 16:15:09 +1100

    Declared a bucket type.

--- gcs/gcsfake/bucket.go
+++ gcs/gcsfake/bucket.go
@@ -3,7 +3,37 @@
 
 package gcsfake
 
-import "github.com/jacobsa/gcloud/gcs"
+import (
+	"io"
+
+	"github.com/jacobsa/gcloud/gcs"
+	"golang.org/x/net/context"
+	"google.golang.org/cloud/storage"
+)
 
 // Create an in-memory bucket with the given name and empty contents.
-func NewFakeBucket(name string) gcs.Bucket
+func NewFakeBucket(name string) gcs.Bucket {
+	return &bucket{name: name}
+}
+
+type bucket struct {
+	name string
+}
+
+func (b *bucket) Name() string
+
+func (b *bucket) ListObjects(
+	ctx context.Context,
+	query *storage.Query) (*storage.Objects, error)
+
+func (b *bucket) NewReader(
+	ctx context.Context,
+	objectName string) (io.ReadCloser, error)
+
+func (b *bucket) NewWriter(
+	ctx context.Context,
+	attrs *storage.ObjectAttrs) (gcs.ObjectWriter, error)
+
+func (b *bucket) DeleteObject(
+	ctx context.Context,
+	name string) error
