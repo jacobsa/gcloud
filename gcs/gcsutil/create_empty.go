9d17c80a4969207a847c671b0ac8c4bc0108a33c
Aaron Jacobs <jacobsa@google.com>
2015-02-13 11:12:24 +1100

    Added a CreateEmptyObjects helper.

---
+++ gcs/gcsutil/create_empty.go
@@ -0,0 +1,29 @@
+// Copyright 2015 Google Inc. All Rights Reserved.
+// Author: jacobsa@google.com (Aaron Jacobs)
+
+package gcsutil
+
+import (
+	"github.com/jacobsa/gcloud/gcs"
+	"golang.org/x/net/context"
+	"google.golang.org/cloud/storage"
+)
+
+// Create empty objects with default attributes for all of the supplied names.
+func CreateEmptyObjects(
+	ctx context.Context,
+	bucket gcs.Bucket,
+	names []string) ([]*storage.Object, error) {
+	// Set up a list of ObjectInfo structs.
+	infoStructs := make([]*ObjectInfo, len(names))
+	for i, name := range names {
+		infoStructs[i] = &ObjectInfo{
+			attrs: storage.ObjectAttrs{
+				Name: name,
+			},
+		}
+	}
+
+	// Defer to CreateObjects.
+	return CreateObjects(ctx, bucket, infoStructs)
+}
