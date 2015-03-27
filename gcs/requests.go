38820f9591f9904c3bd76593e041a40bfa26f25f
Aaron Jacobs <jacobsa@google.com>
2015-03-27 14:27:34 +1100

    Split request structs into a separate file.

---
+++ gcs/requests.go
@@ -0,0 +1,94 @@
+// Copyright 2015 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package gcs
+
+import (
+	"io"
+
+	"google.golang.org/cloud/storage"
+)
+
+// A request to create an object, accepted by Bucket.CreateObject.
+type CreateObjectRequest struct {
+	// Attributes with which the object should be created. The Name field must be
+	// set; other zero-valued fields are ignored.
+	//
+	// Object names must:
+	//
+	// *  be non-empty.
+	// *  be no longer than 1024 bytes.
+	// *  be valid UTF-8.
+	// *  not contain the code point U+000A (line feed).
+	// *  not contain the code point U+000D (carriage return).
+	//
+	// See here for authoritative documentation:
+	//     https://cloud.google.com/storage/docs/bucket-naming#objectnames
+	Attrs storage.ObjectAttrs
+
+	// A reader from which to obtain the contents of the object. Must be non-nil.
+	Contents io.Reader
+
+	// If non-nil, the object will be created/overwritten only if the current
+	// generation for the object name is equal to the given value. Zero means the
+	// object does not exist.
+	GenerationPrecondition *int64
+}
+
+// A request to read the contents of an object at a particular generation.
+type ReadObjectRequest struct {
+	// The name of the object to read.
+	Name string
+
+	// The generation of the object to read. Zero means the latest generation.
+	Generation int64
+}
+
+type StatObjectRequest struct {
+	// The name of the object in question.
+	Name string
+}
+
+// A request to update the metadata of an object, accepted by
+// Bucket.UpdateObject.
+type UpdateObjectRequest struct {
+	// The name of the object to update. Must be specified.
+	Name string
+
+	// String fields in the object to update (or not). The semantics are as
+	// follows, for a given field F:
+	//
+	//  *  If F is set to nil, the corresponding GCS object field is untouched.
+	//
+	//  *  If *F is the empty string, then the corresponding GCS object field is
+	//     removed.
+	//
+	//  *  Otherwise, the corresponding GCS object field is set to *F.
+	//
+	//  *  There is no facility for setting a GCS object field to the empty
+	//     string, since many of the fields do not actually allow that as a legal
+	//     value.
+	//
+	// Note that the GCS object's content type field cannot be removed.
+	ContentType     *string
+	ContentEncoding *string
+	ContentLanguage *string
+	CacheControl    *string
+
+	// User-provided metadata updates. Keys that are not mentioned are untouched.
+	// Keys whose values are nil are deleted, and others are updated to the
+	// supplied string. There is no facility for completely removing user
+	// metadata.
+	Metadata map[string]*string
+}
