c3303e889c413a62d1ae544ec1f826a20855c0cc
Aaron Jacobs <jacobsa@google.com>
2015-03-27 15:05:15 +1100

    Added and documented Listing.

--- gcs/requests.go
+++ gcs/requests.go
@@ -87,13 +87,50 @@ type ListObjectsRequest struct {
 	Delimiter string
 
 	// Used to continue a listing where a previous one left off. See
-	// Listing.Token for more information.
-	Token string
+	// Listing.ContinuationToken for more information.
+	ContinuationToken string
 
-	// The maximum number of objects and collapsed results to return. Fewer than
+	// The maximum number of objects and collapsed runs to return. Fewer than
 	// this number may actually be returned. If this is zero, a sensible default
 	// is used.
 	MaxResults int
+}
+
+// A set of objects and delimter-based collapsed runs returned by a call to
+// ListObjects. See also ListObjectsRequest.
+type Listing struct {
+	// Records for objects matching the listing criteria.
+	//
+	// Guaranteed to be strictly increasing under a lexicographical comparison on
+	// (name, generation) pairs.
+	Objects []Object
+
+	// Collapsed entries for runs of names sharing a prefix followed by a
+	// delimiter. See notes on ListObjectsRequest.Delimiter.
+	//
+	// Guaranteed to be strictly increasing.
+	CollapsedRuns []string
+
+	// A continuation token, for fetching more results.
+	//
+	// If non-empty, this listing does not represent the full set of matching
+	// objects in the bucket. Call ListObjects again with the request's
+	// ContinuationToken field set to this value to continue where you left off.
+	//
+	// Guarantees, for replies R1 and R2, with R2 continuing from R1:
+	//
+	//  *  All of R1's object names are strictly less than all object names and
+	//     collapsed runs in R2.
+	//
+	//  *  All of R1's collapsed runs are strictly less than all object names and
+	//     prefixes in R2.
+	//
+	// (Cf. Google-internal bug 19286144)
+	//
+	// Note that there is no guarantee of atomicity of listings. Objects written
+	// and deleted concurrently with a single or multiple listing requests may or
+	// may not be returned.
+	ContinuationToken string
 }
 
 // A request to update the metadata of an object, accepted by
