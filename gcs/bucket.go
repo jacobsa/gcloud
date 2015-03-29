jacobsa/gcloud

commit bf9535d1e86bace32558ae6ce182960f79eecd16

Consistently use query.Values.
For #4.

gcs/bucket.go
@@ -122,7 +122,11 @@ func (b *bucket) Name() string {
 func (b *bucket) ListObjects(
 	ctx context.Context,
 	req *ListObjectsRequest) (listing *Listing, err error) {
-	// Set up the query for the URL.
+	// Construct an appropriate URL (cf. http://goo.gl/aVSAhT).
+	opaque := fmt.Sprintf(
+		"//www.googleapis.com/storage/v1/b/%s/o",
+		httputil.EncodePathSegment(b.Name()))
+
 	query := make(url.Values)
 	query.Set("projection", "full")
 
@@ -141,11 +145,6 @@ func (b *bucket) ListObjects(
 	if req.MaxResults != 0 {
 		query.Set("maxResults", fmt.Sprintf("%v", req.MaxResults))
 	}
-
-	// Construct an appropriate URL (cf. http://goo.gl/aVSAhT).
-	opaque := fmt.Sprintf(
-		"//www.googleapis.com/storage/v1/b/%s/o",
-		httputil.EncodePathSegment(b.Name()))
 
 	url := &url.URL{
 		Scheme:   "https",
@@ -216,14 +215,14 @@ func (b *bucket) NewReader(
 		bucketSegment,
 		objectSegment)
 
+	query := make(url.Values)
+	if req.Generation != 0 {
+		query.Set("generation", fmt.Sprintf("%v", req.Generation))
+	}
+
 	url := &url.URL{
 		Scheme: "https",
 		Opaque: opaque,
-	}
-
-	// Add a generation condition, if specified.
-	if req.Generation != 0 {
-		url.RawQuery = fmt.Sprintf("generation=%v", req.Generation)
 	}
 
 	// Create an HTTP request.

gcs/create_object.go
@@ -74,17 +74,18 @@ func startResumableUpload(
 		"//www.googleapis.com/upload/storage/v1/b/%s/o",
 		bucketSegment)
 
+	query := make(url.Values)
+	query.Set("projection", "full")
+	query.Set("uploadType", "resumable")
+
+	if req.GenerationPrecondition != nil {
+		query.Set("ifGenerationMatch", fmt.Sprintf("%v", *req.GenerationPrecondition))
+	}
+
 	url := &url.URL{
 		Scheme:   "https",
 		Opaque:   opaque,
-		RawQuery: "uploadType=resumable&projection=full",
-	}
-
-	if req.GenerationPrecondition != nil {
-		url.RawQuery = fmt.Sprintf(
-			"%s&ifGenerationMatch=%v",
-			url.RawQuery,
-			*req.GenerationPrecondition)
+		RawQuery: query.Encode(),
 	}
 
 	// Set up the request body.

commit 4a2fee2b9b79939e8c193d4fac34d061c896ef4e

Fixed a bug introduced in bf9535d1e86bace32558ae6ce182960f79eecd16.

gcs/bucket.go
@@ -219,8 +219,9 @@ func (b *bucket) NewReader(
 	}
 
 	url := &url.URL{
-		Scheme: "https",
-		Opaque: opaque,
+		Scheme:   "https",
+		Opaque:   opaque,
+		RawQuery: query.Encode(),
 	}
 
 	// Create an HTTP request.