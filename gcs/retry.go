39e14c9f888dc5c9918a412e20946300d64c2991
Aaron Jacobs <jacobsa@google.com>
2015-04-01 12:35:25 +1100

    Added an expBackoff helper.

--- gcs/retry.go
+++ gcs/retry.go
@@ -16,9 +16,11 @@ package gcs
 
 import (
 	"io"
+	"math/rand"
 	"time"
 
 	"golang.org/x/net/context"
+	"google.golang.org/api/googleapi"
 )
 
 // A bucket that wraps another, calling its methods in a retry loop with
@@ -29,6 +31,82 @@ type retryBucket struct {
 }
 
 var _ Bucket = &retryBucket{}
+
+////////////////////////////////////////////////////////////////////////
+// Helpers
+////////////////////////////////////////////////////////////////////////
+
+// Exponential backoff for a function that might fail.
+//
+// This is essentially what is described in the "Best practices" section of the
+// "Upload Objects" docs:
+//
+//     https://cloud.google.com/storage/docs/json_api/v1/how-tos/upload
+//
+// with the following exceptions:
+//
+//  *  We perform backoff for all errors except:
+//      *  HTTP 40x errors
+//      *  Error types defined by this package
+//
+//  *  We perform backoff for all operations.
+//
+//  *  The random component scales with the delay, so that the first sleep
+//     cannot be as long as one second. The algorithm used matches the
+//     description at http://en.wikipedia.org/wiki/Exponential_backoff.
+//
+func expBackoff(
+	ctx context.Context,
+	f func() error,
+	maxSleep time.Duration) (err error) {
+	const baseDelay = time.Millisecond
+	var totalSleep time.Duration
+
+	for n := uint(0); ; n++ {
+		// Make an attempt.
+		err = f()
+
+		// Is this an error we want to pass through?
+		if _, ok := err.(*NotFoundError); ok {
+			return
+		}
+
+		if _, ok := err.(*PreconditionError); ok {
+			return
+		}
+
+		if typed, ok := err.(*googleapi.Error); ok {
+			if typed.Code >= 400 && typed.Code < 500 {
+				return
+			}
+		}
+
+		// Choose a a delay in [0, 2^n * baseDelay).
+		d := (1 << n) * baseDelay
+		d = time.Duration(float64(d) * rand.Float64())
+
+		// Are we out of credit?
+		if totalSleep+d > maxSleep {
+			// Return the most recent error.
+			return
+		}
+
+		// Sleep, returning early if cancelled.
+		select {
+		case <-ctx.Done():
+			err = ctx.Err()
+			return
+
+		case <-time.After(d):
+			totalSleep += d
+			continue
+		}
+	}
+}
+
+////////////////////////////////////////////////////////////////////////
+// Public interface
+////////////////////////////////////////////////////////////////////////
 
 func (rb *retryBucket) Name() (name string) {
 	name = rb.wrapped.Name()
