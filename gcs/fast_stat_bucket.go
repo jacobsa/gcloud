8b359482a6ec2a9def9b155cc2a00297d89c2d88
Aaron Jacobs <jacobsa@google.com>
2015-04-28 13:44:10 +1000

    Declared NewFastStatBucket.

---
+++ gcs/fast_stat_bucket.go
@@ -0,0 +1,30 @@
+// Copyright 2015 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package gcs
+
+import (
+	"time"
+
+	"github.com/googlecloudplatform/gcsfuse/timeutil"
+)
+
+// Create a bucket that caches object records returned by the supplied wrapped
+// bucket. Records are invalidated when modifications are made through this
+// bucket, and after the supplied TTL.
+func NewFastStatBucket(
+	ttl time.Duration,
+	cache StatCache,
+	clock timeutil.Clock,
+	wrapped Bucket) (b Bucket)
