f723903cbdf504870ed40368dc3e0da97412d8ab
Aaron Jacobs <jacobsa@google.com>
2015-06-09 15:52:18 +1000

    Check for valid UTF-8.

--- gcs/copy_object.go
+++ gcs/copy_object.go
@@ -16,11 +16,13 @@ package gcs
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
 	"strings"
+	"unicode/utf8"
 
 	"github.com/jacobsa/gcloud/httputil"
 	"google.golang.org/api/googleapi"
@@ -32,6 +34,14 @@ import (
 func (b *bucket) CopyObject(
 	ctx context.Context,
 	req *CopyObjectRequest) (o *Object, err error) {
+	// We encode using json.NewEncoder, which is documented to silently transform
+	// invalid UTF-8 (cf. http://goo.gl/3gIUQB). So we can't rely on the server
+	// to detect this for us.
+	if !utf8.ValidString(req.DstName) {
+		err = errors.New("Invalid object name: not valid UTF-8")
+		return
+	}
+
 	// Construct an appropriate URL (cf. https://goo.gl/A41CyJ).
 	opaque := fmt.Sprintf(
 		"//www.googleapis.com/storage/v1/b/%s/o/%s/copyTo/b/%s/o/%s",
