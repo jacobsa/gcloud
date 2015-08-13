ceec972b96d2d257735ece50f750e140af28fc8e
Aaron Jacobs <jacobsa@google.com>
2015-08-13 22:11:56 +1000

    Deleted error specs that are likely to rot.
    Some already have.

--- gcs/bucket.go
+++ gcs/bucket.go
@@ -56,9 +56,6 @@ type Bucket interface {
 	// eventually for listing) after this method returns a nil error. It is
 	// guaranteed not to exist before req.Contents returns io.EOF.
 	//
-	// If the request fails due to a precondition not being met, the error will
-	// be of type *PreconditionError.
-	//
 	// Official documentation:
 	//     https://cloud.google.com/storage/docs/json_api/v1/objects/insert
 	//     https://cloud.google.com/storage/docs/json_api/v1/how-tos/upload
@@ -70,8 +67,6 @@ type Bucket interface {
 	// generation of the destination name will be overwritten.
 	//
 	// Returns a record for the new object.
-	//
-	// If the source object doesn't exist, err will be of type *NotFoundError.
 	//
 	// Official documentation:
 	//     https://cloud.google.com/storage/docs/json_api/v1/objects/copy
@@ -85,10 +80,6 @@ type Bucket interface {
 	//
 	// Returns a record for the new object.
 	//
-	// If any of the sources don't exist, err will be of type *NotFoundError. If
-	// the request fails due to a precondition not being met, the error will be
-	// of type *PreconditionError.
-	//
 	// Official documentation:
 	//     https://cloud.google.com/storage/docs/json_api/v1/objects/compose
 	ComposeObjects(
@@ -96,8 +87,6 @@ type Bucket interface {
 		req *ComposeObjectsRequest) (*Object, error)
 
 	// Return current information about the object with the given name.
-	//
-	// If the object doesn't exist, err will be of type *NotFoundError.
 	//
 	// Official documentation:
 	//     https://cloud.google.com/storage/docs/json_api/v1/objects/get
@@ -118,8 +107,6 @@ type Bucket interface {
 
 	// Update the object specified by newAttrs.Name, patching using the non-zero
 	// fields of newAttrs.
-	//
-	// If the object doesn't exist, err will be of type *NotFoundError.
 	//
 	// Official documentation:
 	//     https://cloud.google.com/storage/docs/json_api/v1/objects/patch
